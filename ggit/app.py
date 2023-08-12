89aab5f1ed75c059c215cb75ba31ee8368d87685
mxblsdl <maxblasdel@gmail.com>
2023-08-11 21:12:24 -0700

    update to get and add fetch

--- ggit/app.py
+++ ggit/app.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 import subprocess
 import typer
-from helpers import check_config
+from helpers import check_config, pull_git_element, create_table
+from rich.progress import track
+
 
 app = typer.Typer(
     help="Check all git repos",
@@ -11,26 +13,77 @@ app = typer.Typer(
 )
 
 
-@app.command()
-def get():
+@app.command("get")
+# Make remove_clean into an argument
+def get(
+    filter_clean: bool = typer.Option(False, "--filter_clean", "-fc", is_flag=True)
+):
     config = check_config()
 
     fp = Path(config["ROOT"]).expanduser()
-    for filename in fp.glob("**/.git"):
+
+    branch = []
+    commit_status = []
+    modified = []
+    project = []
+
+    for filename in track(fp.glob("**/.git"), description="Looking..."):
         parent_folder = Path(filename).parent
         res = subprocess.run(
-            ["git", "-C", parent_folder, "status"], stdout=subprocess.PIPE
+            ["git", "-C", parent_folder, "status"], capture_output=True, text=True
         )
+        project.append(parent_folder.stem)
+
         # process results into nicer format
-        print(res.stdout.splitlines())
+        lines = res.stdout.splitlines()
+
+        # Filter out empty lines
+        lines = [l for l in lines if not l.startswith(" ") if l != ""]
+
+        if filter_clean:
+            if any("nothing to commit" in l for l in lines):
+                continue
+
+        # Filter out specific elements from lines
+        branch.append(pull_git_element(lines, "On branch", "On branch"))
+        commit_status.append(pull_git_element(lines, "commit", "(.*"))
+        modified.append(pull_git_element(lines, "\t", "\tmodified: "))
+
+    # create table
+    data = list(zip(project, branch, commit_status, modified))
+
+    create_table(data)
 
 
-@app.command()
-def echo():
-    print("test")
+# This one is hard to test since fetch isn't returning anything for me repos and consumes its output...
+@app.command("fetch")
+def fetch(prune: bool = typer.Option(False, "--prune", "p", is_flag=True)):
+    config = check_config()
+    fp = Path(config["ROOT"]).expanduser()
+
+    project = []
+
+    for filename in track(fp.glob("**/.git"), description="Fetching..."):
+        project.append(parent_folder.stem)
+        parent_folder = Path(filename).parent
+        if prune:
+            res = subprocess.run(
+                ["git", "-C", parent_folder, "fetch", "-p"],
+                capture_output=True,
+                text=True,
+            )
+        else:
+            res = subprocess.run(
+                ["git", "-C", parent_folder, "fetch"],
+                capture_output=True,
+                text=True,
+            )
+    lines = res.stdout.splitlines()
+
+    # Filter out empty lines
+    lines = [l for l in lines if not l.startswith(" ") if l != ""]
+    print(lines)
 
 
-# TODO process results and display in table
-# Have option to discard clean statuses
-
-typer.run(get)
+# Have an option to fetch and prune
+typer.run(fetch)
--- ggit/helpers.py
+++ ggit/helpers.py
@@ -1,11 +1,14 @@
 from pathlib import Path
 import typer
+from rich.prompt import Confirm
+from rich.console import Console
+from rich.table import Table
 
 
 def create_config(config_file: Path):
-    typer.confirm(
-        "No config file exists yet\nShould we create one?", abort=True, default=None
-    )
+    res = Confirm.ask("No config file exists yet\nShould we create one?")
+    if not res:
+        raise typer.Abort()
 
     config_file.touch()
 
@@ -36,3 +39,32 @@ def check_config():
     if not config_file.exists():
         create_config(config_file)
         raise typer.Abort()
+
+
+def pull_git_element(lines: list, el_str: str, rep_str: str, els: int = 0) -> str:
+    el = [l for l in lines if el_str in l]
+    if not el:
+        return ''
+
+    return el[els].replace(rep_str, "")
+
+
+def create_table(data: list[list]) -> None:
+        table = Table(title="Global Git Status", show_lines=True)
+
+        table.add_column("Project", justify="center", style="blue")
+        table.add_column("Current Branch", style="magenta")
+        table.add_column("Commits", justify="center", style="green")
+        table.add_column("Modified", justify="left", style="on dodger_blue2")
+
+
+        for d in data:
+            table.add_row(
+                d[0],
+                d[1],
+                d[2],
+                d[3],
+            )
+
+        console = Console()
+        console.print(table)
